f2957ca79e0a10fd45f253fc71e9c3a6bf954100
erodriguez96 <eduardo.rdgz@hotmail.com>
2019-04-11 11:24:51 +0100

    upload images and get its data done

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,11 @@
+export const FirebaseConfig = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA53R3tqv2u6YQG_H4UcOI5y3Seci8uQ_8",
+    authDomain: "cloudinaryproject.firebaseapp.com",
+    databaseURL: "https://cloudinaryproject.firebaseio.com",
+    projectId: "cloudinaryproject",
+    storageBucket: "cloudinaryproject.appspot.com",
+    messagingSenderId: "459285123814"
+  }
+};
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,37 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { CloudinaryOptions, CloudinaryUploader } from "ng2-cloudinary";
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
+  uploader: CloudinaryUploader;
+  loading: any;
+
   constructor(public navCtrl: NavController) {
+    this.uploader  = new CloudinaryUploader(
+      new CloudinaryOptions({ cloudName: 'erh96', uploadPreset: 'kraivfb1' })
+    );
+  }
+
+  upload(){
+    console.log("hola");
+    this.loading = true;
+    this.uploader.uploadAll();
+    this.uploader.onSuccessItem = (item: any, response: string, status: number, headers: any): any => {
+      let res: any = JSON.parse(response);
+      console.log(res);
+    }
+    this.uploader.onErrorItem = function(fileItem, response, status, headers) {
+      console.info('onErrorItem', fileItem, response, status, headers);
+    };
+  }
+
+  retrieveImage(){
 
   }
 
